5fdba94f753df1314b0996c6520cac117caff2e6
imrane-bendassi <146710804+ImraneBendassi@users.noreply.github.com>
2024-05-11 15:04:52 +0200

    import du composants carte

--- memory/App.js
+++ memory/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Cards from './composants/carte.js';
 
 function App() {
   return (
     <div className="App">
       <h1>Jeu Memory - Avec React</h1>
+      <Cards />
       <div className="container"></div>
     </div>
   );
 }
 
 export default App;
-
